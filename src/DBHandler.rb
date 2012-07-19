f9f9b57dd6e0ed74ca98dc9478fe3b63c9891d2e
Gregory Maglio <gmaglio@gmail.com>
2012-07-18 20:26:53 -0400

    Preparation for repush to github

--- src/DBHandler.rb
+++ src/DBHandler.rb
@@ -1,16 +1,12 @@
 #!/usr/bin/env /usr/bin/ruby
-<<<<<<< HEAD
 
 # Author: Gregory Maglio
 # DBHandler.rb: Object to handle DB transactions via sqlite.
 
-=======
->>>>>>> 203a634da23cb022cd4c298844e902eecdd9d384
 require 'sqlite3'
 
 class DBHandler 
 
-<<<<<<< HEAD
   def initialize(mode = "RO")
      if( mode.match("RW") ) 
          @db = SQLite3::Database.new("../data/glm.db")
@@ -19,12 +15,6 @@ class DBHandler
          @db = SQLite3::Database.new("../data/glm.db")
          @db.execute("create table if not exists HIGH_SCORES(name TEXT, score INTEGER, date INTEGER, PRIMARY KEY(name, score, date))")
      end
-=======
-  def initialize
-     @db = SQLite3::Database.new("../data/glm.db")
-     @db.execute("drop table if exists HIGH_SCORES")
-     @db.execute("create table HIGH_SCORES(name TEXT, score INTEGER, date INTEGER, PRIMARY KEY(name, score, date))")
->>>>>>> 203a634da23cb022cd4c298844e902eecdd9d384
   end
 
   def insert_rec(name, score, date)
@@ -38,11 +28,7 @@ class DBHandler
      return caughtExcept
   end
 
-<<<<<<< HEAD
   def get_records
      @db.execute("select * from HIGH_SCORES;")
   end
-
-=======
->>>>>>> 203a634da23cb022cd4c298844e902eecdd9d384
 end
--- src/HighScore.rb
+++ src/HighScore.rb
@@ -1,18 +1,14 @@
 #!/usr/bin/env ruby
 
-<<<<<<< HEAD
 # Author: Gregory Maglio
 # HighScore.rb: Object to define HighScore as a data structure.
 
-=======
->>>>>>> 203a634da23cb022cd4c298844e902eecdd9d384
 class HighScore
 
      attr_accessor :name 
      attr_accessor :score 
      attr_accessor :date
 
-<<<<<<< HEAD
      def initialize(name = '0', score = 0, date = 0)
           @name = Gtk::Entry.new
           @name.text = name
@@ -28,12 +24,6 @@ class HighScore
 
      def to_str
           "#{@name.text} #{@score.text.to_i} #{@date.text.to_i}"
-=======
-     def initialize
-          @name = Gtk::Entry.new
-          @score = Gtk::Entry.new
-          @date = Gtk::Entry.new
->>>>>>> 203a634da23cb022cd4c298844e902eecdd9d384
      end
 
 end
